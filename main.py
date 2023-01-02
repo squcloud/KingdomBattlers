7e57deaa2f48165453f480ec870634fe67999b68
squcloud <jniemirski@gmail.com>
2023-01-02 14:00:28 +0100

    Added player 2 full interface

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import sleep
 occupied = "cannot move here because this place is occupied"
 
 
-map_size = [19, 25]
+map_size = [19, 28]
 row_text=[]
 for i in range(map_size[1]):
     row_text.append(" ")
@@ -29,6 +29,9 @@ class Player:
         for i in self.castle_list:
             hp += i.hp
         return hp
+    def check_income(self):
+        income = len(self.castle_list) * 250 + len(self.economy_building_list) * 75
+        return income
 
 
 player_list = []
@@ -39,7 +42,6 @@ player_list.append(player2)
 
 
 
-castles = []
 class Map_Row:
     def __init__(self, id):
         self.id = id
@@ -187,15 +189,11 @@ class Castle:
         for x in range(self.width):
             for y in range(self.height):
               set_object(headshift(self.position[0], x), self.position[1] + y, " ")
+        player_list[self.player-1].castle_list.remove(self)
+
         del self
 
-def create_castle(player, position, size, hp):
-    Castle.id += 1
-    player.castles_list.append(Castle(player, position, size, hp))
-    for x in range(size[0]):
-        for y in range(size[1]):
-            set_object(headshift(position[0], x), position[1] + y, castles[Castle.id - 1])
-    
+
 
 
 
@@ -216,7 +214,8 @@ map_header = ""
 map_header += "        "
 for i in list(map(chr, range(65, 65 + map_size[0]))):
     map_header += i + "   "
-#map_header = ["R |  A   B   C   D   E   F   G   H   I   J   |"]
+#map_header = ["R |  A   B   C   D   E   F   G   H   I   J   |"] =
+map_header += "                               KINGDOM BATTLERS            "
 row_list = []
 for i in range(map_size[1]):
     row_list.append(Map_Row(i))
@@ -243,7 +242,7 @@ row_list[9].col["F"] = "X" """
 def print_map():
     print()
     print(map_header)
-    print("      " + map_size[0] * 4 * "-" + "-" + "              KINGDOM BATTLERS")
+    print("      " + map_size[0] * 4 * "-" + "-" + "   " + "-" * 74)
     for row in row_list:
         row.print_row()
     print("      " + map_size[0] * 4 * "-" + "-")
@@ -347,7 +346,7 @@ def attack(walker): # mechanics of prompting for attack of soldier
 
 
 #testing 
-soldier1 = Soldier(1, x="C", y=5)
+soldier1 = Soldier(1, x="C", y=5, ap=20)
 soldier2 = Soldier(2, x="F", y=19)
 soldier3 = Soldier(1, x="N", y=11)
 soldier4 = Soldier(1, x="H", y=10)
@@ -358,7 +357,7 @@ soldier7 = Soldier(1, x="K", y=13)
 #soldier2.xy("I", 9)
 
 castle1 = Castle(1,["H",1], [4, 2], 50)
-castle2 = Castle(2,["H",22], [4, 2], 50)
+castle2 = Castle(2,["H",25], [4, 2], 50)
 castle3 = Castle(1,["C",15], [4, 2], 50)
 tower1 = Tower(1,10,4,x="D",y=5)
 tower2 = Tower(1,10,4,x="B",y=8)
@@ -372,32 +371,64 @@ economy2 = Economy_building(2,1,x="B",y=22)
 
 # INTERFACE SECTION
 def row_text_update(index):
-    if index == 1:
-        row_text[index] = "   Name: \033[34m" + player_list[0].name.upper() + "\033[0m" + "  Castle HP: \033[34m" + str(player1.check_hp()) + "\033[00m / \033[34m" + str(len(player1.castle_list)*50) + "\033[00m" + "  Gold: " + "\033[34m" + str(player_list[0].gold) + "\033[00m    Income:"
-    elif index == 3:
+    if index == 0:
+        row_text[index] = "   Name: \033[34m" + player_list[0].name.upper() + "\033[0m" + "  Castle HP: \033[34m" + str(player1.check_hp()) + "\033[00m / \033[34m" + str(len(player1.castle_list)*50) + "\033[00m" + "  Gold: " + "\033[34m" + str(player_list[0].gold) + "\033[00m $    Income: \033[34m" + str(player1.check_income()) + "\033[00m $"
+    elif index == 2:
         row_text[index] = "   Soldiers:"
-    elif index == 4:
+    elif index == 3:
         row_text[index] = "  "
         for i in range(3):
             if len(player1.soldier_list)>= i+1:
                 row_text[index] += "[" + str(i+1)  + " HP: \033[34m" + str(player1.soldier_list[i].hp) + "\033[00m/\033[34m10\033[00m " + "Pos: \033[34m" + str(player1.soldier_list[i].x) + "\033[00m:\033[34m" + str(player1.soldier_list[i].y) + "\033[00m]  "
-    elif index == 5:
+    elif index == 4:
         row_text[index] = "  "
         for i in range(3,6):
             if len(player1.soldier_list)>= i+1:
                 row_text[index] += "[" + str(i+1)  + " HP: \033[34m" + str(player1.soldier_list[i].hp) + "\033[00m/\033[34m10\033[00m " + "Pos: \033[34m" + str(player1.soldier_list[i].x) + "\033[00m:\033[34m" + str(player1.soldier_list[i].y) + "\033[00m]  "
-    elif index == 7:
+    elif index == 6:
         row_text[index] = "   Towers:"
-    elif index == 8:
+    elif index == 7:
         row_text[index] = "  "
         for i in range(3):
+            if len(player1.tower_list)>= i+1:
+                row_text[index] += "[" + str(i+1)  + " HP: \033[34m" + str(player1.tower_list[i].hp) + "\033[00m/\033[34m10\033[00m " + "Pos: \033[34m" + str(player1.tower_list[i].x) + "\033[00m:\033[34m" + str(player1.tower_list[i].y) + "\033[00m]  "
+    elif index == 8:
+        row_text[index] = "  "
+        for i in range(3,6):
             if len(player1.tower_list)>= i+1:
                 row_text[index] += "[" + str(i+1)  + " HP: \033[34m" + str(player1.tower_list[i].hp) + "\033[00m/\033[34m10\033[00m " + "Pos: \033[34m" + str(player1.tower_list[i].x) + "\033[00m:\033[34m" + str(player1.tower_list[i].y) + "\033[00m]  "
     elif index == 9:
+        row_text[index] = "  " + "-" * 73
+ 
+    # Interface for sexond player:
+    elif index == 10:
+        row_text[index] = "   Name: \033[31m" + player_list[1].name.upper() + "\033[0m" + "  Castle HP: \033[31m" + str(player2.check_hp()) + "\033[00m / \033[31m" + str(len(player2.castle_list)*50) + "\033[00m" + "  Gold: " + "\033[31m" + str(player_list[1].gold) + "\033[00m $    Income: \033[31m" + str(player2.check_income()) + "\033[00m $"
+    elif index == 12:
+        row_text[index] = "   Soldiers:"
+    elif index == 13:
+        row_text[index] = "  "
+        for i in range(3):
+            if len(player2.soldier_list)>= i+1:
+                row_text[index] += "[" + str(i+1)  + " HP: \033[31m" + str(player2.soldier_list[i].hp) + "\033[00m/\033[31m10\033[00m " + "Pos: \033[31m" + str(player2.soldier_list[i].x) + "\033[00m:\033[31m" + str(player2.soldier_list[i].y) + "\033[00m]  "
+    elif index == 14:
         row_text[index] = "  "
         for i in range(3,6):
-            if len(player1.tower_list)>= i+1:
-                row_text[index] += "[" + str(i+1)  + " HP: \033[34m" + str(player1.tower_list[i].hp) + "\033[00m/\033[34m10\033[00m " + "Pos: \033[34m" + str(player1.tower_list[i].x) + "\033[00m:\033[34m" + str(player1.tower_list[i].y) + "\033[00m]  "
+            if len(player2.soldier_list)>= i+1:
+                row_text[index] += "[" + str(i+1)  + " HP: \033[31m" + str(player2.soldier_list[i].hp) + "\033[00m/\033[31m10\033[00m " + "Pos: \033[31m" + str(player2.soldier_list[i].x) + "\033[00m:\033[31m" + str(player2.soldier_list[i].y) + "\033[00m]  "
+    elif index == 16:
+        row_text[index] = "   Towers:"
+    elif index == 17:
+        row_text[index] = "  "
+        for i in range(3):
+            if len(player2.tower_list)>= i+1:
+                row_text[index] += "[" + str(i+1)  + " HP: \033[31m" + str(player2.tower_list[i].hp) + "\033[00m/\033[31m10\033[00m " + "Pos: \033[31m" + str(player2.tower_list[i].x) + "\033[00m:\033[31m" + str(player2.tower_list[i].y) + "\033[00m]  "
+    elif index == 18:
+        row_text[index] = "  "
+        for i in range(3,6):
+            if len(player2.tower_list)>= i+1:
+                row_text[index] += "[" + str(i+1)  + " HP: \033[31m" + str(player2.tower_list[i].hp) + "\033[00m/\033[31m10\033[00m " + "Pos: \033[31m" + str(player2.tower_list[i].x) + "\033[00m:\033[31m" + str(player2.tower_list[i].y) + "\033[00m]  "
+    elif index == 19:
+        row_text[index] = "  " + "-" * 73
 
 
 
